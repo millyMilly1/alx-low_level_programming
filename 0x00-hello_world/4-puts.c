a393fdf5c970581f3c1fd234bfa84e71e0994c24
millyMilly1 <chukwurahenry@gmail.com>
2022-10-13 16:26:57 -0700

    (C program 01)

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ *
+ * *main-printing
+ *
+ * *
+ *
+ * *Return: always 0 (successful)
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"programming is like buliding a multilingual puzzle");
+
+	return(0);
+
+}
