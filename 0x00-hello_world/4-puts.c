9e1600c763154a7558bb5242e0537ca76376266f
millyMilly1 <chukwurahenry@gmail.com>
2022-10-13 18:28:44 -0700

    (task 04 level C)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
+
+
+
 /**
- * *main-printing_in_c
  *
- * *Return: always 0 (successful program)
+ * * main - Printing
+ *
+ * *
+ *
+ * * Return: Always 0 (Successful)
+ *
  * */
 
 int main(void)
+
 {
-	puts("\"programming is like buliding a multilingual puzzle");
-	return(0);
+
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
+	return (0);
+
 }
